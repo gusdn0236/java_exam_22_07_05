f5219557b7a84a4d76e9e6971dbf82f5df7a8c2b
gusdn0236 <hdno0236@naver.com>
2022-07-05 23:13:49 +0900

    곱하기 기능 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -2,25 +2,35 @@ package com.ll.exam;
 
 public class Calculator {
 
-    public static int run(String s) {
+    public static int calc(String s) {
+        boolean isMulti = s.indexOf(" * ") != -1;
         boolean isMinus = s.indexOf(" - ") != -1;
 
         if(isMinus){
-            return runMinus(s);
-        }else{
-            return runPlus(s);
+            return calcMinus(s);
+        }else if(isMulti){
+            return calcMulti(s);
+        } else{
+            return calcPlus(s);
         }
 
     }
 
-    private static int runPlus(String s) {
+    private static int calcMulti(String s) {
+        String[] sBits = s.split(" \\* ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+        return no1 * no2;
+    }
+
+    private static int calcPlus(String s) {
         String[] sBits = s.split(" \\+ ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
         return no1 + no2;
     }
 
-    private static int runMinus(String s) {
+    private static int calcMinus(String s) {
         String[] sBits = s.split(" \\- ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
@@ -29,3 +39,4 @@ public class Calculator {
 
 
 }
+
