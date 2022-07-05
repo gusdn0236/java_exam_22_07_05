4212653c333c12208e30f187536716a6c9ad2d55
gusdn0236 <hdno0236@naver.com>
2022-07-05 13:44:14 +0900

    add함수명을 run으로 수정

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -2,10 +2,13 @@ package com.ll.exam;
 
 public class Calculator {
 
-    public static int add(String s) {
+    public static int run(String s) {
       String[] sBits = s.split(" \\+ ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
         return no1 + no2;
     }
+
+
+
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,24 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void 더하기_테스트() {
-       int rs = Calculator.add("10 + 20");
+    public void 더하기() {
+        int rs = Calculator.run("10 + 20");
 
-       assertEquals(30,rs);
+        assertEquals(30, rs);
     }
 
     @Test
-    public void 더하기_테스트_2() {
-        int rs = Calculator.add("20 + 20");
+    public void 더하기_2() {
+        int rs = Calculator.run("20 + 20");
 
-        assertEquals(40,rs);
+        assertEquals(40, rs);
     }
 
     @Test
-    public void 더하기_테스트_3() {
-        int rs = Calculator.add("10 + 10");
+    public void 더하기_3() {
+        int rs = Calculator.run("10 + 10");
 
-        assertEquals(20,rs);
+        assertEquals(20, rs);
     }
+
+
+
 }
 
